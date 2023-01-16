9359eea1386ef91ad3d697b1d7502a32207755cd
marc0sfelipe <marcos12004@hotmail.com>
2023-01-16 16:57:09 -0300

    Loops

---
+++ exer_while.py
@@ -0,0 +1,9 @@
+nome = 'Marcos Felipe'
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+   letra = nome[indice]
+   novo_nome += letra
+   indice += 1 
+
+print (novo_nome)
---
+++ exerc_if.py
@@ -0,0 +1,11 @@
+number_1 = input('Digite um numero. ')
+number_2 = input('Digite outro numero. ')
+
+int_number_1 = int(number_1)
+int_number_2 = int(number_2)
+
+if int_number_1 > int_number_2:
+    print (f'o numero {int_number_1} é maior doq o numero {int_number_2}')
+
+else: 
+    print(f'o numero {int_number_2} é maior doq o numero {int_number_1}')
